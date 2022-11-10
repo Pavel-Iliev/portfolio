d79d2df5ea03e12673675e7e1f606bf2b85499c1
Pavel Iliev <p88iliev@gmail.com>
2022-11-10 07:41:39 +0100

    add media query hook

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ThemeSwitch from '../hooks/ThemeSwitch';
 import styles from '../../styles/Header.module.scss';
+import Typewriter from 'typewriter-effect';
+import UseMediaQuery from '../hooks/UseMediaQuery';
 
 type Classes = {
   classes: string;
 };
 
+const hiWords: string[] = ['Hi,', 'Ciao,', 'Здравеи,', 'Hallo,', 'Hola,', 'Bonjour', 'こんにちは,'];
+
 export default function Header({ classes }: Classes) {
+  // const isMobile = UseMediaQuery(600);
+  // const hamburgerMenu = isMobile && 'sticazzi';
   return (
     <>
       <section className={styles[classes]}>
@@ -14,6 +20,16 @@ export default function Header({ classes }: Classes) {
           <h1>Header</h1>
           <p>Text paragraph</p>
           <ThemeSwitch />
+          <h2>
+            <Typewriter
+              options={{
+                strings: hiWords,
+                delay: 200,
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
         </div>
       </section>
     </>
