65c078cef9207f52c28e645b04ce04a68ebc846d
Pavel Iliev <p88iliev@gmail.com>
2022-11-20 18:37:09 +0100

    fix text header and hover btn

--- pages/components/Header/Presentation.tsx
+++ pages/components/Header/Presentation.tsx
@@ -5,21 +5,6 @@ import { HI_WORDS } from '../../../utils/constants';
 import { labels } from '../../../utils/labels';
 
 export default function Presentation() {
-  function presentationMeNewLine(
-    word: string,
-    tag: React.DetailedHTMLProps<React.HTMLAttributes<HTMLBRElement>, HTMLBRElement>,
-  ) {
-    const presentationMe = labels.presentation_me.split(word);
-    let result: (string | React.DetailedHTMLProps<React.HTMLAttributes<HTMLBRElement>, HTMLBRElement>)[] = [];
-    presentationMe.forEach((w, i) => {
-      result.push(w);
-      if (i < presentationMe.length - 1) {
-        result.push(tag);
-      }
-    });
-    return result;
-  }
-
   return (
     <div className={styles.presentation}>
       <h3 className={styles.presentation__hi}>
@@ -34,11 +19,9 @@ export default function Presentation() {
       </h3>
       <div className={styles.presentation__me}>
         <h1>
-          <>
-            {presentationMeNewLine('[BR]', <br />).map((el, i) =>
-              el && typeof el === 'string' ? <span key={i}>{el}</span> : el,
-            )}
-          </>
+          {labels.i_am_pavel}
+          <br />
+          {labels.full_stack}
         </h1>
         <p>{labels.front_end_passion}</p>
       </div>
--- utils/labels.js
+++ utils/labels.js
@@ -1,5 +1,6 @@
 export const labels = {
-  presentation_me: `I'm Pavel, [BR] Full-stack Developer`,
+  i_am_pavel: "I'm Pavel",
+  full_stack: 'Full-stack Developer',
   contact_me: 'Contact me!',
   front_end_passion: 'With passion for the Front-end and UI.',
 };
